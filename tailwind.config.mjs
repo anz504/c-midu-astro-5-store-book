7fe00e34575dd26e87521f6d0db5fa018e56927c
adain_navarro <adain_navarro@hotmail.com>
2026-01-04 21:45:30 +0100

    [FEAT] Installing tailwind + books imgs

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+//** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,json,md,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
